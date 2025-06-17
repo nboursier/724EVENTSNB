0747ec9294955b287da3b4aa861843c11cb36db9
nboursier <nicolasboursier@yahoo.fr>
2025-06-17 15:41:51 +0200

    first commit

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,16 @@
+export const MONTHS = [
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "août",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre",
+];
+
+export const getMonth = (date) => MONTHS[date.getMonth()];
